7d6fee602cd394ec7c139f2191462003331d5d95
Abhigyan Trips <contact@abhigyantrips.dev>
2026-04-12 14:00:38 +0530

    chore: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare(),
+  integrations: [react()],
+});
